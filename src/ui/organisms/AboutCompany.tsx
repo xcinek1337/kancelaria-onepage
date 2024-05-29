ebdf424a999f1ce63cbcbc26c3ba44641526d63c
Marcin <marcinchodkowski004@wp.pl>
2024-05-29 16:08:58 +0200

    rwd AboutCompany

--- src/ui/organisms/AboutCompany.tsx
+++ src/ui/organisms/AboutCompany.tsx
@@ -18,19 +18,25 @@ const AboutCompanyCards = [
 export const AboutCompany = () => {
 	return (
 		<section>
-			<div className="px-5 pt-10 text-center">
-				<p className="font-sans text-sm font-medium italic text-gray-500">
+			<div className="px-5 pt-10 text-center lg:pt-16">
+				<p className="font-sans text-sm font-medium italic text-gray-500 md:text-base">
 					Witaj w naszej Kancelarii
 				</p>
-				<div className="bg-brownie mx-auto mt-4 h-1 w-12 rounded-md"></div>
-				<h2 className="font-playfair pt-4 text-3xl ">Doświadczenie. Zespół. Rezultaty</h2>
-				<p className="mt-8 px-4 font-serif text-sm tracking-wide text-gray-500">
+
+				<div className="bg-brownie mx-auto mt-4 h-1 w-12 rounded-md md:w-20"></div>
+
+				<h2 className="font-playfair mt-4 text-4xl md:text-5xl lg:mt-8 lg:text-7xl ">
+					Doświadczenie. Zespół. Rezultaty.
+				</h2>
+
+				<p className="mt-8 px-4 font-serif text-sm tracking-wide text-gray-500 md:text-base lg:mt-12 lg:text-lg">
 					Stawiamy sobie za zadanie dostarczanie naszym klientom usług na najwyższym poziomie
-					jakości.
+					jakości
 				</p>
-				<div className="border-brownie mt-10 flex flex-col justify-center gap-14 rounded-sm  border-8 py-6 md:flex-row">
+
+				<div className="border-brownie mx-auto mt-10 lg:mt-16 flex flex-col justify-center gap-14 rounded-sm border-8 px-2 py-6 sm:gap-20  md:w-[90%] md:flex-row lg:w-[75%]">
 					{AboutCompanyCards.map((card, i) => (
-						<div key={i} className="flex  flex-col items-center ">
+						<div key={i} className="flex flex-col  items-center md:w-1/3 ">
 							<p className="font-playfair text-6xl">{card.title}</p>
 							<p className="mt-2 text-sm text-gray-500">{card.description}</p>
 						</div>
